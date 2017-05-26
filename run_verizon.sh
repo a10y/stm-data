c4fb1748f5b3ec789f5bc47d3b0c8fae701711c7
Harrison Ho <harrison8989@gmail.com>
2017-05-25 22:01:07 -0700

    Fix hanging issue

--- run_verizon.sh
+++ run_verizon.sh
@@ -1,3 +1,3 @@
 # Usage: sh run_verizon.sh <mosh or ssh> <keyword> <input trace file>
 
-mm-link /usr/share/mahimahi/traces/Verizon-LTE-short.up /usr/share/mahimahi/traces/Verizon-LTE-short.down ./term-replay-client $3 $2 $1 192.168.171.10 "cd stm-data && ./term-replay-server $3"
+mm-delay 20 mm-link /usr/share/mahimahi/traces/Verizon-LTE-short.up /usr/share/mahimahi/traces/Verizon-LTE-short.down ./term-replay-client $3 $2 $1 192.168.171.10 "cd stm-data && ./term-replay-server $3"
