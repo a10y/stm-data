d9a23640f0ed8658ad6baa40904b5a1f9b113b65
Andrew Duffy <agd@cs.stanford.edu>
2017-05-26 06:01:25 +0000

    Added traces

--- run_verizon.sh
+++ run_verizon.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 # Usage: sh run_verizon.sh <mosh or ssh> <keyword> <input trace file>
+[[ "$(sysctl net.ipv4.ip_forward)" =~ "0" ]] && sudo sysctl -w net.ipv4.ip_forward=1
 
-mm-delay 20 mm-link /usr/share/mahimahi/traces/Verizon-LTE-short.up /usr/share/mahimahi/traces/Verizon-LTE-short.down ./term-replay-client $3 $2 $1 "192.168.171.10 -- sh -c \"cd stm-data && ./term-replay-server $3\""
+mm-delay 20 mm-link ./traces/Verizon-LTE-short.up ./traces/Verizon-LTE-short.down ./term-replay-client $3 $2 $1 "192.168.171.10 -- sh -c \"cd stm-data && ./term-replay-server $3\""
