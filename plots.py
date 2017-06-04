b12e30c7b55a13ae60fed473d694b93bc3b2e264
Andrew Duffy <agd@cs.stanford.edu>
2017-06-04 09:12:26 +0000

    Fix plotting

--- plots.py
+++ plots.py
@@ -15,40 +15,31 @@ def gen_plot(session, scale):
     # Read the dataset from argv[1]
     mosh_fname = "%s.mosh.delay" % session
     ssh_fname = "%s.ssh.delay" % session
-    
+
     # Read values from file
-    mosh_values = read_values(mosh_fname)
-    ssh_values = read_values(ssh_fname)
-    
-    # binning
-    mosh_vals, mosh_base = np.histogram(mosh_values, bins=100)
-    ssh_vals, ssh_base = np.histogram(ssh_values, bins=100)
-    
-    
-    mosh_cum = np.cumsum(mosh_vals)
-    ssh_cum = np.cumsum(ssh_vals)
-    
-    print mosh_vals
-    print mosh_cum
-    
+    mosh_values = sorted(read_values(mosh_fname))
+    ssh_values = sorted(read_values(ssh_fname))
+
+    mosh_ys = [float(i+1)/len(mosh_values) for i in range(len(mosh_values))]
+    ssh_ys = [float(i+1)/len(ssh_values) for i in range(len(ssh_values))]
+
     plt.figure()
-    
+
     plt.axis(scale)
-    
-    ax = plt.gca()
-    ax.hist(mosh_values, 100, normed=1, histtype='step', cumulative=True, label='Mosh')
-    ax.hist(ssh_values, 100, normed=1, histtype='step', cumulative=True, label='SSH')
-    
+
+    plt.plot(mosh_values, mosh_ys, label='Mosh')
+    plt.plot(ssh_values, ssh_ys, label='SSH')
+
     #plt.plot(mosh_base[:-1], mosh_vals, c='blue', label='Mosh')
     #plt.plot(ssh_base[:-1], ssh_vals, c='red', label='SSH')
-    
+
     plt.xlabel('Keystroke response time (s)')
     plt.ylabel('Percentage')
     plt.title('Cumulative Distribution of Keystroke Response Times Using Verizon LTE Traces')
     plt.legend(loc=4)
-    
+
     plt.savefig('%s-plot.png' % session)
 
 gen_plot('high_delay', [0.0, 7.0, 0, 1.0])
-gen_plot('high_loss', [0.0, 4.0, 0, 1.0])
+gen_plot('high_loss', [0.0, 2.0, 0, 1.0])
 gen_plot('figure2', [0.0, 1.0, 0, 1.0])
